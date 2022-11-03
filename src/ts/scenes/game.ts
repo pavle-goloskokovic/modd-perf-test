8eea5320e3913ea3c5235fef1722163fa11b821e
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2022-11-03 16:22:23 +0100

    lint; config updates

--- src/ts/scenes/game.ts
+++ src/ts/scenes/game.ts
@@ -19,59 +19,64 @@ export default class Game extends Phaser.Scene {
   animals: (Pig | Bear)[] = [];
   players: Player[] = [];
 
-  create(): void {
-    logger.info('Game enter');
+  create (): void
+  {
+      logger.info('Game enter');
 
-    this.cameras.main.setZoom(5 / 11);
+      this.cameras.main.setZoom(5 / 11);
 
-    const map = this.make.tilemap({
-      width: TILEMAP_SIZE,
-      height: TILEMAP_SIZE,
-      tileWidth: TILE_SIZE,
-      tileHeight: TILE_SIZE
-    });
+      const map = this.make.tilemap({
+          width: TILEMAP_SIZE,
+          height: TILEMAP_SIZE,
+          tileWidth: TILE_SIZE,
+          tileHeight: TILE_SIZE
+      });
 
-    const tiles = map.addTilesetImage('tilesheet_complete', null, TILE_SIZE, TILE_SIZE);
+      const tiles = map.addTilesetImage('tilesheet_complete', null, TILE_SIZE, TILE_SIZE);
 
-    const indexes: number[] = [];
-    for (let i = -1; i < 540; i++) {
-      indexes.push(i);
-    }
-    ['floor', 'floor2', 'debris', 'walls', 'trees'].forEach(layerName => {
-      const layer = map.createBlankLayer(layerName, tiles);
-      layer.randomize(0, 0, map.width, map.height, indexes);
+      const indexes: number[] = [];
+      for (let i = -1; i < 540; i++)
+      {
+          indexes.push(i);
+      }
+      ['floor', 'floor2', 'debris', 'walls', 'trees'].forEach(layerName =>
+      {
+          const layer = map.createBlankLayer(layerName, tiles);
+          layer.randomize(0, 0, map.width, map.height, indexes);
 
       /*console.log(layer);
             console.log(layer.layer);*/
-    });
+      });
 
-    const items = ['blood', 'rocks', 'tree', 'log', 'wall', 'meat', 'helmet', 'fur'];
-    this.players.push(
-      new Player(this, Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE, Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE)
-    );
-    this.cameras.main.startFollow(this.players[0].entity);
-
-    for (let i = 0; i < STATIC_ENTITIES_SIZE; i++) {
-      const item = this.add.image(
-        Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
-        Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
-        items[Math.floor(Math.random() * items.length)]
+      const items = ['blood', 'rocks', 'tree', 'log', 'wall', 'meat', 'helmet', 'fur'];
+      this.players.push(
+          new Player(this, Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE, Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE)
       );
-      item.setOrigin(0.5);
-      item.setScale(Math.min(TILE_SIZE / item.width, TILE_SIZE / item.height));
-    }
+      this.cameras.main.startFollow(this.players[0].entity);
 
-    for (let i = 0; i < ACTIVE_ENTITIES_SIZE; i++) {
-      this.animals.push(
-        new (Math.random() < 0.5 ? Pig : Bear)(
-          this,
-          Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
-          Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE
-        )
-      );
-    }
+      for (let i = 0; i < STATIC_ENTITIES_SIZE; i++)
+      {
+          const item = this.add.image(
+              Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
+              Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
+              items[Math.floor(Math.random() * items.length)]
+          );
+          item.setOrigin(0.5);
+          item.setScale(Math.min(TILE_SIZE / item.width, TILE_SIZE / item.height));
+      }
 
-    /* this.players.push(
+      for (let i = 0; i < ACTIVE_ENTITIES_SIZE; i++)
+      {
+          this.animals.push(
+              new (Math.random() < 0.5 ? Pig : Bear)(
+                  this,
+                  Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
+                  Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE
+              )
+          );
+      }
+
+      /* this.players.push(
                 new Player(
                     this,
                     Math.floor(Math.random() * TILEMAP_SIZE) * TILE_SIZE,
@@ -79,7 +84,7 @@ export default class Game extends Phaser.Scene {
                 )
             ); */
 
-    /*const cursors = this.input.keyboard.createCursorKeys();
+      /*const cursors = this.input.keyboard.createCursorKeys();
         this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
             camera: this.cameras.main,
             left: cursors.left,
@@ -98,15 +103,18 @@ export default class Game extends Phaser.Scene {
         help.setScrollFactor(0);*/
   }
 
-  update(time: number, delta: number): void {
-    // this.controls.update(delta);
+  update (time: number, delta: number): void
+  {
+      // this.controls.update(delta);
 
-    this.animals.forEach(a => {
-      a.update(time, delta);
-    });
+      this.animals.forEach(a =>
+      {
+          a.update(time, delta);
+      });
 
-    this.players.forEach(a => {
-      a.update(time, delta);
-    });
+      this.players.forEach(a =>
+      {
+          a.update(time, delta);
+      });
   }
 }
